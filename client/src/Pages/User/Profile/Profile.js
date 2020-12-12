d8b28ca394142bc49c64b9a75fadf170840c4265
mathias <mathiasandre1998@gmail.com>
2020-12-12 15:05:56 +0100

    Some ui changes to /profile

--- client/src/Pages/User/Profile/Profile.js
+++ client/src/Pages/User/Profile/Profile.js
@@ -5,9 +5,50 @@ export default class Profile extends Component {
   render() {
     return (
       <div>
-        <h1>
-          this is the profile page, it can be found in /pages/user/profile
-        </h1>
+        <div className="profile-container">
+          <div className="left-box">
+            {/*Profile image */}
+            <img src="" id="user-image" alt="" />
+            {/*Profile Name */}
+            <h1 id="user-name">Name</h1>
+            {/*Keyboard layout  Default QWERTY*/}
+            <h2 id="user-keyboardlayout">QWERTY</h2>
+            {/*Current typing speed */}
+            <h3 id="user-avrage-speed">speed - wpm</h3>
+          </div>
+          <div className="right-box">
+            {/*User statistic */}
+            <div className="personal-score">
+              <h1>Personal Top</h1>
+              <table>
+                <tr>
+                  <th>Time</th>
+                  <th>Wpm</th>
+                  <th>Raw</th>
+                  <th>Accuracy</th>
+                </tr>
+                <tr className="grey">
+                  <td>15</td>
+                  <td>30</td>
+                  <td>94</td>
+                  <td>60%</td>
+                </tr>
+                <tr>
+                  <td>30</td>
+                  <td>40</td>
+                  <td>94</td>
+                  <td>60%</td>
+                </tr>
+                <tr className="grey">
+                  <td>60</td>
+                  <td>50</td>
+                  <td>94</td>
+                  <td>60%</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
