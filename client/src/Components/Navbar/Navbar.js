b4097a99ee75620b44f6fdb8a041c02a4b2d6711
mathias <mathiasandre1998@gmail.com>
2020-12-13 14:11:57 +0100

    change

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -3,15 +3,13 @@ import React from "react";
 import styled from "styled-components";
 
 const NavContainer = styled.div`
-  width: 100%;
-  height: 100px;
-  display: flex;
+
 `;
 
 const Navbar = (props) => {
   return(
     <NavContainer>
-    
+  
     </NavContainer>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import Home from './Pages/Home/Components/Home';
+import Navbar from './Components/Navbar/Navbar'
 import Signup from "./Pages/Signup/Components/Signup"; // Temporary links to Signup for now. TO change back later.
 import Login from "./Pages/Login/Components/Login";
 ReactDOM.render(
   <React.StrictMode>
-    <Signup />
+    <Navbar/>
+    <Login />
   </React.StrictMode>,
   document.getElementById("root")
 );
