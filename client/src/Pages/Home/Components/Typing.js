6d6d4800915cd6b9c00902755f414368389409ae
Azzpr <mathiasandre1998@gmail.com>
2020-12-31 03:20:18 +0100

    Adedd a random quote api

--- client/src/Pages/Home/Components/Typing.js
+++ client/src/Pages/Home/Components/Typing.js
@@ -1,23 +1,39 @@
 // External modules
 import React from 'react';
 import styled from 'styled-components';
-
+const axios = require('axios').default;
 //Internal modules
-console.log('gey');
+
+//Getting a random quote
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
+//using axios to get the quote
+axios
+    .get(RANDOM_QUOTE_API_URL)
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
 
 const TypingContainer = styled.div`
-width: 100%;
-height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 const InputContainer = styled.div`
-width: 80%; 
-height: 60%;
-position: absolute;
-left: 50%;
-transform: translateX(-50%);
-border: 2px solid red;
-
-input{
+    width: 80%;
+    height: 60%;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    border: 2px solid red;
+`;
+const TextArea = styled.textarea`
     width: 900px;
     margin: auto;
     display: block;
@@ -25,16 +41,27 @@ input{
     color: white;
     border: 3px solid white;
     border-radius: 3px;
-}
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+const QuoteArea = styled.div`
+    border: 2px solid red;
+    width: 900px;
+    margin: auto;
+    height: 30%;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
 `;
 const Typing = (props) => {
     return (
-    <TypingContainer>
-        <InputContainer>
-        <input />
-        </InputContainer>
-        
-    </TypingContainer>
+        <TypingContainer>
+            <InputContainer>
+                <QuoteArea id="QuoteArea">TypingChamp - Speed typing</QuoteArea>
+                <TextArea />
+            </InputContainer>
+        </TypingContainer>
     );
 };
 
